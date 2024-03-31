f2e5631f6c472863f4473b57920ce78826ca8a85
harysusilo50 <harysusilo50@gmail.com>
2024-04-01 05:07:52 +0700

    fix migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Carbon::setLocale('id'); // Set locale to Indonesian
         setlocale(LC_TIME, 'id_ID'); // Set locale for month and day names
     }
